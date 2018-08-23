2b849bd82e939b4d85e8e036931475671f6a9b7d
Viswa Sunny <viswasunny007@gmail.com>
2018-08-23 09:47:41 +0530

    Create 82.java

---
+++ 82.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		Scanner s=new Scanner(System.in);
+		double l=s.nextFloat();
+		double b=s.nextFloat();
+		double area=l*b;
+		System.out.printf("%.5f",area);
+	}
+}
