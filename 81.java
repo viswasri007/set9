fd7d189c7e05517a4f188edbd9fddd48fba4ee54
Viswa Sunny <viswasunny007@gmail.com>
2018-08-23 09:45:25 +0530

    Create 81.java

---
+++ 81.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int k=s.nextInt();
+		int o=s.nextInt();
+		System.out.print(Math.abs(k-o));
+		/*if(k<o)
+		{
+			System.out.print(o-k);
+		}
+		else
+		System.out.print("Invalid");*/
+		
+	}
+}
