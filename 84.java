b31c64a288f2882bc6e2d8c82c81ee52b2a3b4b4
Viswa Sunny <viswasunny007@gmail.com>
2018-08-23 09:56:49 +0530

    Create 84.java

---
+++ 84.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Guvi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		Scanner s=new Scanner(System.in);
+		char ch=s.next().charAt(0);
+		System.out.print((int)ch);
+	}
+}
